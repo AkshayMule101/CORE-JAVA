a10b319647a667e2760fef4b989265be9123472b
AkshayMule101 <67921032+AkshayMule101@users.noreply.github.com>
2020-10-06 20:18:01 +0530

    Add files via upload

--- Assignment_3/Q32.java
+++ Assignment_3/Q32.java
@@ -31,11 +31,11 @@
  }
  
 // main method
-	class Que32{
+	class Q32{
 		public static void main(String ab[])
 		{
 			Student s1=new Student();
-			s1.setData(1,"Anjali badgujar");
+			s1.setData(1,"Akshay Mule");
 			s1.showData();
 			Student s2=new Student();
 			Student s3=new Student();
